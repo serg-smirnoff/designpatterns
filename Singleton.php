da4bc4848585978fdd2ed2bbbb8ddbadf4ffb358
root <root@onlysites.ru>
2015-08-20 12:41:34 +0300

    Dessign Patterns. Commit 3

--- Singleton.php
+++ Singleton.php
@@ -6,39 +6,6 @@
  * and open the template in the editor.
  */
 
-echo "class Preferences {
-    
-    private $props = array();
-    
-    private static $instance;                       // статическое свойство
-    
-    private function __construct() {}               // закрытый конструктор. теперь нельзя создать экземпляр класса из вне
-        
-    public static function getInstance()            // статический метод
-    {
-      if ( empty ( self::$instance ) ) {
-          self::$instance = new Preferences();
-      }
-      return self::$instance;
-    }
-    
-    public function setProperty($key, $val){
-        $this->props[$key] = $val;
-    }
-    
-    public function getProperty($key){
-        return $this->props[$key];
-    }
-    
-}
-
-$pref = Preferences::getInstance();
-$pref->setProperty( \"name\", \"Иван\" );
-unset ($pref);
-
-$pref2 = Preferences::getInstance();
-print $pref2->getProperty( \"name\" );";
-
 class Preferences {
     
     private $props = array();
