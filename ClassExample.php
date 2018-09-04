c9e7bff8b607706ccfe9c2718574425a7a23555f
root <info@serg-smirnoff.com>
2018-09-04 15:48:52 +0300

    change codename to utf8

---
+++ ClassExample.php
@@ -0,0 +1,27 @@
+<?php
+
+class Test{
+	
+	public $publicProperty;
+	private	$privateProperty;
+	protected $protectedProperty;
+	
+	public function __construct(){
+		return $this->privateProperty = 4;
+	}
+	
+	public function publicMethod() {
+		return $this->publicProperty + $this->privateProperty;
+	}
+	
+	public function __destruct(){
+		
+	}
+	
+}
+
+$test = new Test();
+$test->publicProperty = 2;
+echo $test->publicMethod();
+
+?>
